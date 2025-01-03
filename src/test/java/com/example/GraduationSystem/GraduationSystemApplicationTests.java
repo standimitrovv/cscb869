6584b426d6bd055405212789c59b745a5fe5098f
Stanimir Dimitrov <evlution63@gmail.com>
2025-01-03 09:51:43 +0200

    App Init

---
+++ src/test/java/com/example/GraduationSystem/GraduationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GraduationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
