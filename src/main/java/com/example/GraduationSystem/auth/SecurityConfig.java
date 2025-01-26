f272c54d763ba1fcbea4e5d8bdb4f70eec43dbfa
Stanimir Dimitrov <evlution63@gmail.com>
2025-01-26 16:48:44 +0200

    chore: routes protection

--- src/main/java/com/example/GraduationSystem/auth/SecurityConfig.java
+++ src/main/java/com/example/GraduationSystem/auth/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.GraduationSystem.auth;
 
+import com.example.GraduationSystem.model.user.UserRole;
 import com.example.GraduationSystem.service.SessionService;
 import com.example.GraduationSystem.utils.PasswordEncoderUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,32 +31,41 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/**").permitAll()
-//                        .requestMatchers("/session/**").permitAll()
-//                        .requestMatchers("/").permitAll()
-//                        .requestMatchers("/home").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/lecturers/{lecturerId}").hasRole(UserRole.LECTURER.toString())
-//                        .requestMatchers("/lecturers/**").hasRole(UserRole.ADMIN.toString())
-//                        .requestMatchers(HttpMethod.GET, "/students/{studentId}").hasRole(UserRole.STUDENT.toString())
-//                        .requestMatchers("/students/**").hasRole(UserRole.ADMIN.toString())
-//                        .requestMatchers(HttpMethod.POST, "/theses/{thesisRequestId}").hasRole(UserRole.STUDENT.toString())
-//                        .requestMatchers(HttpMethod.GET, "/theses/**").hasRole(UserRole.LECTURER.toString())
-//                        .requestMatchers( "/theses/**").hasRole(UserRole.ADMIN.toString())
-//                        .requestMatchers(HttpMethod.GET, "/thesisDefenses/{defenseId}").hasRole(UserRole.STUDENT.toString())
-//                        .requestMatchers(HttpMethod.POST, "/thesisDefenses/**").hasRole(UserRole.LECTURER.toString())
-//                        .requestMatchers(HttpMethod.PATCH, "/thesisDefenses/**").hasRole(UserRole.LECTURER.toString())
-//                        .requestMatchers(HttpMethod.DELETE, "/thesisDefenses/**").hasRole(UserRole.LECTURER.toString())
-//                        .requestMatchers("/thesisDefenses/**").hasRole(UserRole.ADMIN.toString())
-//                        .requestMatchers(HttpMethod.GET, "/thesisRequests/approved").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/thesisRequests/{thesisRequestId}").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/thesisRequests/students/{studentId}").permitAll()
-//                        .requestMatchers(HttpMethod.POST, "/thesisRequests").hasRole(UserRole.LECTURER.toString())
-//                        .requestMatchers(HttpMethod.PATCH, "/thesisRequests/{thesisRequestId}").hasRole(UserRole.LECTURER.toString())
-//                        .requestMatchers(HttpMethod.DELETE, "/thesisRequests/{thesisRequestId}").hasRole(UserRole.LECTURER.toString())
-//                        .requestMatchers("/thesisRequests/**").hasRole(UserRole.ADMIN.toString())
-//                        .requestMatchers(HttpMethod.POST, "/thesisReviews").hasRole(UserRole.LECTURER.toString())
-//                        .requestMatchers(HttpMethod.PATCH, "/thesisReviews/**").hasRole(UserRole.LECTURER.toString())
-//                        .requestMatchers( "/thesisReviews/**").hasRole(UserRole.ADMIN.toString())
+                        .requestMatchers("/session/**").permitAll()
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/home").permitAll()
+                        .requestMatchers("/theses").hasRole(UserRole.STUDENT.toString())
+                        .requestMatchers("/theses/details/{id}").permitAll()
+                        .requestMatchers("/theses/upload").hasRole(UserRole.STUDENT.toString())
+                        .requestMatchers("/theses/unreviewed").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers("/theses/lecturer/pending").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers("/thesisRequests").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers("/thesisRequests/student").hasRole(UserRole.STUDENT.toString())
+                        .requestMatchers("/thesisReviews/assign").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers("/thesisReviews/upload").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers("/api/session/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/lecturers/{lecturerId}").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers("/api/lecturers/**").hasRole(UserRole.ADMIN.toString())
+                        .requestMatchers(HttpMethod.GET, "/api/students/{studentId}").hasRole(UserRole.STUDENT.toString())
+                        .requestMatchers("/api/students/**").hasRole(UserRole.ADMIN.toString())
+                        .requestMatchers(HttpMethod.POST, "/api/theses/{thesisRequestId}").hasRole(UserRole.STUDENT.toString())
+                        .requestMatchers(HttpMethod.GET, "/api/theses/**").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers( "/api/theses/**").hasRole(UserRole.ADMIN.toString())
+                        .requestMatchers(HttpMethod.GET, "/api/thesisDefenses/{defenseId}").hasRole(UserRole.STUDENT.toString())
+                        .requestMatchers(HttpMethod.POST, "/api/thesisDefenses/**").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers(HttpMethod.PATCH, "/api/thesisDefenses/**").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers(HttpMethod.DELETE, "/api/thesisDefenses/**").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers("/api/thesisDefenses/**").hasRole(UserRole.ADMIN.toString())
+                        .requestMatchers(HttpMethod.GET, "/api/thesisRequests/approved").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/thesisRequests/{thesisRequestId}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/thesisRequests/students/{studentId}").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/thesisRequests").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers(HttpMethod.PATCH, "/api/thesisRequests/{thesisRequestId}").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers(HttpMethod.DELETE, "/api/thesisRequests/{thesisRequestId}").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers("/api/thesisRequests/**").hasRole(UserRole.ADMIN.toString())
+                        .requestMatchers(HttpMethod.POST, "/api/thesisReviews").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers(HttpMethod.PATCH, "/api/thesisReviews/**").hasRole(UserRole.LECTURER.toString())
+                        .requestMatchers( "/api/thesisReviews/**").hasRole(UserRole.ADMIN.toString())
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
